4cdf441a925523ea4cf00b3d2a7c4500a35cc5d5
Jais Cheema <jais@trikeapps.com>
2013-09-11 07:39:39 +1000

    Start implementing devise authentication

--- config/routes.rb
+++ config/routes.rb
@@ -1,20 +1,7 @@
 Invoices::Application.routes.draw do
   devise_for :users
-  root 'sessions#new'
-
-  controller :sessions do
-    get "login", :action => :new, :as => :show_login
-    post "login", :action => :create, :as => :do_login
-    delete "logout", :action => :destroy, :as => :logout
-  end
-
+  root 'home#index'
   resources :invoices
   resources :clients
   resources :companies
-  resources :users
-
-  controller :users do
-    get "sign_up", :action => :sign_up, :as => :sign_up
-    post "sign_up", :action => :public_create, :as => :public_user_create
-  end
 end
