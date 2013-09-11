9925bc9c171a638e0850d387845be251ed6cc08c
Jais Cheema <jaischeema@gmail.com>
2013-09-11 17:33:01 +1000

    Set the home page to the login page without redirect

--- config/routes.rb
+++ config/routes.rb
@@ -1,6 +1,14 @@
 Invoices::Application.routes.draw do
+
   devise_for :users
-  root 'home#index'
+
+  devise_scope :user do
+    root "devise/sessions#new"
+    delete "/logout" => "devise/sessions#destroy", as: :logout
+  end
+
+  get '/dashboard' => 'dashboard#index', as: :dashboard
+
   resources :invoices
   resources :clients
   resources :companies
