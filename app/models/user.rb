8bbed188d0c2c7a7d98a61c40b3aeae1571e68cb
Jais Cheema <jaischeema@gmail.com>
2013-09-11 21:32:23 +1000

    Remove user registration module from devise

--- app/models/user.rb
+++ app/models/user.rb
@@ -1,4 +1,4 @@
 class User < ActiveRecord::Base
-  devise :database_authenticatable, :registerable, :recoverable, :rememberable, :trackable
+  devise :database_authenticatable, :recoverable, :rememberable, :trackable
   belongs_to :company
 end
