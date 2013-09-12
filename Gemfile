2fb460b6586361e39b1b5dfedd9f015e76f0b662
Jais Cheema <jais@trikeapps.com>
2013-09-12 10:24:09 +1000

    Add ability to load site config from environment variables

--- Gemfile
+++ Gemfile
@@ -4,11 +4,14 @@ ruby '2.0.0'
 
 gem 'rails', '4.0.0'
 
-gem 'sqlite3', group: :development
-
 group :production do
   gem 'rails_12factor'
   gem 'pg'
+end
+
+group :development do
+  gem 'sqlite3'
+  gem 'foreman'
 end
 
 gem 'sass-rails', '~> 4.0.0'
--- config/initializers/load_app_config.rb
+++ config/initializers/load_app_config.rb
@@ -1,6 +1,11 @@
 require 'ostruct' # To convert configuration to object
 
-raw_config = File.read("#{Rails.root}/config/app_config.yml")
-APP_CONFIG = YAML.load(raw_config)[Rails.env].symbolize_keys
+APP_CONFIG =
+  if File.exists?("#{Rails.root}/config/app_config.yml")
+    raw_config = File.read("#{Rails.root}/config/app_config.yml")
+    YAML.load(raw_config)[Rails.env].symbolize_keys
+  else
+    { :site_title => ENV['SITE_TITLE'], :domain => ENV['SITE_DOMAIN']}
+  end
 
 AppConfig = OpenStruct.new(APP_CONFIG)
